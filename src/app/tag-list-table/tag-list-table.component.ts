4ef1b9355fe22c9fa0a6f2c4847b9ff8f749d1a7
Andrew Giangrant <andrew.giangrant@outlook.com>
2017-10-11 20:31:29 -0400

    add filtering mechanism

--- src/app/tag-list-table/tag-list-table.component.ts
+++ src/app/tag-list-table/tag-list-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TagService } from '../services/tag.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tag-list-table',
@@ -8,9 +9,28 @@ import { TagService } from '../services/tag.service';
 })
 export class TagListTableComponent implements OnInit {
   rows:Array<any> = this.tagService.tags;
-  constructor(public tagService:TagService) { }
+  filteredText = "";
+  constructor(public tagService:TagService, private router:Router) { }
 
   ngOnInit() {
   }
 
+  public changeFilter(data:any, filterString:string):any {
+    let filteredData:Array<any> = data;
+    filteredData = filteredData.filter((item:any) => {
+      // if we are filtering by features it needs to filter through each feature
+        return item.features.filter((feature:any) => {
+          return feature.match(filterString);
+        }).length > 0;
+    });
+
+    return filteredData;
+  }
+
+  onSearchChange(filterString, searchText):any {
+    //notice we use tagService to filter
+    let filteredData = this.changeFilter(this.tagService.tags, this.filteredText);
+    // reassign rows
+    this.rows = filteredData;
+  }
 }
