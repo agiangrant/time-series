ebde78c61e78f153aa7c66d408d1d7ff6c433aa1
Andrew Giangrant <andrew.giangrant@outlook.com>
2017-10-11 23:30:13 -0400

    add tests to tag-list-table component

--- src/app/tag-list-table/tag-list-table.component.spec.ts
+++ src/app/tag-list-table/tag-list-table.component.spec.ts
@@ -22,4 +22,20 @@ describe('TagListTableComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('filteredText should filter the input data', () => {
+    expect(component.changeFilter(["what", "do", "you", "think"], "wha")).toBe(["what"]);
+  });
+
+  it('filteredText should not return all input data if nothing matches', () => {
+    expect(component.changeFilter(["what", "do", "you", "think"], "zwqqq?") === ["what", "do", "you", "think"]).toBeFalsy();
+  });
+
+  it('filteredText should return all when no filteredText is passed', () => {
+    expect(component.changeFilter(["what", "do", "you", "think"], "") === ["what", "do", "you", "think"]).toBeTruthy();
+  });
+
+  it('filteredText should return all if text matches all the data passed in', () => {
+    expect(component.changeFilter(["Testing", "Breaking", "Fixing"], "ing") === ["Testing", "Breaking", "Fixing"]).toBeTruthy();
+  })
 });
