cef821f15c13468ce7b19b40522cbdea2435a0f4
Andrew Giangrant <andrew.giangrant@outlook.com>
2017-10-11 20:06:36 -0400

    add ng2-toastr to toast to user when we have errors

--- src/app/services/tag.service.ts
+++ src/app/services/tag.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Tag } from '../interfaces/tag';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Injectable()
 export class TagService {
-    constructor(private http:Http) {
+    constructor(private http:Http, private toastr:ToastsManager) {
         this.fetch();
     }
     tags:Tag[];
@@ -16,6 +17,8 @@ export class TagService {
         },
         error => {
             console.log(error);
+            // let's toast the user with a notification if something went wrong
+            this.toastr.error("An error occurred while attempting to get Tag data.", "ERROR!");
         });
     }
 
