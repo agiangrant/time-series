aaf0e630193e544ecaaab6b652738825aa405064
Andrew Giangrant <andrew.giangrant@outlook.com>
2017-10-12 20:33:18 -0400

    use mock services for TagService

--- src/app/tag-list-table/tag-list-table.component.spec.ts
+++ src/app/tag-list-table/tag-list-table.component.spec.ts
@@ -4,8 +4,7 @@ import { TagListTableComponent } from './tag-list-table.component';
 import { Router, RouterLink } from '@angular/router';
 import { TagService } from '../services/tag.service';
 import { FormsModule } from '@angular/forms';
-import { Http, ConnectionBackend, HttpModule } from '@angular/http';
-import { ToastsManager, ToastOptions } from 'ng2-toastr/ng2-toastr';
+import { Injectable } from '@angular/core';
 
 
 describe('TagListTableComponent', () => {
@@ -15,11 +14,15 @@ describe('TagListTableComponent', () => {
   let mockRouter = {
     navigate: jasmine.createSpy('navigate')
   };
+
+  @Injectable()
+  class MockTagService {}
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ TagListTableComponent, RouterLink ],
-      imports: [FormsModule, HttpModule],
-      providers: [{provide: Router, useValue: mockRouter}, TagService, Http, ConnectionBackend, ToastsManager, ToastOptions]
+      imports: [FormsModule],
+      providers: [{provide: Router, useValue: mockRouter}, {provide: TagService, useValue: MockTagService}]
     })
     .compileComponents();
   }));
@@ -35,18 +38,19 @@ describe('TagListTableComponent', () => {
   });
 
   it('filteredText should filter the input data', () => {
-    expect(component.changeFilter(["what", "do", "you", "think"], "wha")).toBe(["what"]);
+    console.log(component)
+    expect(component.changeFilter([{features:["what"]}, {features:["do"]}, {features:["you"]}, {features:["think"]}], "wha").length).toBe(1);
   });
 
   it('filteredText should not return all input data if nothing matches', () => {
-    expect(component.changeFilter(["what", "do", "you", "think"], "zwqqq?") === ["what", "do", "you", "think"]).toBeFalsy();
+    expect(component.changeFilter([{features:["what"]}, {features:["do"]}, {features:["you"]}, {features:["think"]}], "zqwww?").length).toBe(0);
   });
 
   it('filteredText should return all when no filteredText is passed', () => {
-    expect(component.changeFilter(["what", "do", "you", "think"], "") === ["what", "do", "you", "think"]).toBeTruthy();
+    expect(component.changeFilter([{features:["what"]}, {features:["do"]}, {features:["you"]}, {features:["think"]}], "").length).toBe(4);
   });
 
   it('filteredText should return all if text matches all the data passed in', () => {
-    expect(component.changeFilter(["Testing", "Breaking", "Fixing"], "ing") === ["Testing", "Breaking", "Fixing"]).toBeTruthy();
+    expect(component.changeFilter([{features:["Testing"]}, {features:["Breaking"]}, {features:["Fixing"]}], "ing").length).toBe(3);
   })
 });
